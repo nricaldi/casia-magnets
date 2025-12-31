30778488c1cbc6ad1e6126e2f77175d76492f1f9
Nico Ricaldi <nricaldi.nr@gmail.com>
2025-12-31 18:49:08 -0400

    Enable redirect after successful sign in

--- apps/web/app/auth/actions.ts
+++ apps/web/app/auth/actions.ts
@@ -2,7 +2,6 @@
 
 import { createClient } from "../../lib/supabase/server";
 import { redirect } from 'next/navigation'
-import { headers } from 'next/headers';
 
 export async function signUpNewUser(formData: FormData) {
   const supabase = await createClient();
@@ -28,10 +27,6 @@ export async function signInWithEmail(formData: FormData) {
   const supabase = await createClient();
   const email = formData.get('email') as string;
   const password = formData.get('password') as string;
-  const origin = headers().get('origin');
-  const options = {
-    redirectTo: `${origin}`
-  };
 
   const { data, error } = await supabase.auth.signInWithPassword({ email, password });
 
@@ -45,6 +40,6 @@ export async function signInWithEmail(formData: FormData) {
     console.log({ data });
     console.log('url:', data.url);
 
-    // redirect('/gallery');
+    redirect('/gallery');
   }
 }
