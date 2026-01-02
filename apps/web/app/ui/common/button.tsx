a4af3167d1bc1e65855ae6c5d70e41536bb77217
Nico Ricaldi <nricaldi.nr@gmail.com>
2026-01-02 16:17:58 -0400

    Run prettier. Fix build issues

--- apps/web/app/ui/common/button.tsx
+++ apps/web/app/ui/common/button.tsx
@@ -3,44 +3,34 @@ import Link from 'next/link';
 import { clsx } from 'clsx';
 import styles from './button.module.css';
 
-const sizeClasses = {
-  lg: styles.sizeLg,
-  sm: styles.sizeSm,
-};
+const sizeClasses = { lg: styles.sizeLg, sm: styles.sizeSm };
 
-const variantClasses = {
-  light: styles.variantLight,
-  dark: styles.variantDark,
-  accent: styles.variantAccent
-};
+const variantClasses = { light: styles.variantLight, dark: styles.variantDark, accent: styles.variantAccent };
 
 type ButtonProps = {
   as?: 'button' | 'link';
   variant?: keyof typeof variantClasses;
   size?: keyof typeof sizeClasses;
+  children: React.ReactNode;
   href?: string;
   icon?: ReactNode;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-export default function Button({ as = 'button', variant='light', size='lg', children, href, icon, onClick }: ButtonProps) {
-  const classNames = clsx(
-    styles.button,
-    variantClasses[variant],
-    sizeClasses[size]
-  );
+export default function Button({ as = 'button', variant = 'light', size = 'lg', children, href, icon, onClick }: ButtonProps) {
+  const classNames = clsx(styles.button, variantClasses[variant], sizeClasses[size]);
 
   if (as === 'link') {
     return (
-      <Link href={href} className={classNames}>
-        { children } { icon }
+      <Link href={href ?? '#'} className={classNames}>
+        {children} {icon}
       </Link>
-     );
+    );
   }
 
   return (
     <button className={classNames} onClick={onClick}>
-      { children }
+      {children}
     </button>
   );
 }
