48415b036f0d3948b66d3ab24bb730cbcf0d7298
Nico Ricaldi <nricaldi.nr@gmail.com>
2026-01-26 18:10:58 -0500

    Add loading and disabled state to button

--- apps/web/app/ui/common/button.tsx
+++ apps/web/app/ui/common/button.tsx
@@ -1,11 +1,15 @@
 import { ReactNode } from 'react';
+import { LuLoaderCircle } from 'react-icons/lu';
 import Link from 'next/link';
 import { clsx } from 'clsx';
 import styles from './button.module.css';
 
 const sizeClasses = { lg: styles.sizeLg, sm: styles.sizeSm };
-
-const variantClasses = { light: styles.variantLight, dark: styles.variantDark, accent: styles.variantAccent };
+const variantClasses = {
+  light: styles.variantLight,
+  dark: styles.variantDark,
+  accent: styles.variantAccent
+};
 
 type ButtonProps = {
   as?: 'button' | 'link';
@@ -15,9 +19,21 @@ type ButtonProps = {
   href?: string;
   icon?: ReactNode;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
+  isLoading?: boolean;
 };
 
-export default function Button({ as = 'button', variant = 'light', size = 'lg', children, href, icon, onClick }: ButtonProps) {
+export default function Button({
+  as = 'button',
+  variant = 'light',
+  size = 'lg',
+  disabled = false,
+  isLoading = false,
+  children,
+  href,
+  icon,
+  onClick
+}: ButtonProps) {
   const classNames = clsx(styles.button, variantClasses[variant], sizeClasses[size]);
 
   if (as === 'link') {
@@ -29,7 +45,13 @@ export default function Button({ as = 'button', variant = 'light', size = 'lg', 
   }
 
   return (
-    <button className={classNames} onClick={onClick}>
+    <button className={classNames} disabled={disabled} onClick={onClick}>
+      {isLoading && (
+        <span className={styles.loadingOverlay}>
+          <LuLoaderCircle className={styles.loadingIcon} />
+        </span>
+      )}
+
       {children}
     </button>
   );
